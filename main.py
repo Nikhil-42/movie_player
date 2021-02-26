e4aec126a1cd9de21508ab170f50e68ee4bfda41
Nikhil-Work <53100819+Nikhil-Work@users.noreply.github.com>
2021-02-26 01:47:22 -0500

    variable color space (compile time)

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import cv2
 from numba import jit
-from numba.types import uint8
 from scipy import spatial
-import pandas as pd
 import numpy as np
-import hsluv
 from progressbar import progressbar
 import pathlib
 
+color_space = cv2.COLOR_BGR2LAB
 avg_kern = np.ones((3, 2)) / 6
 
 @jit(forceobj=True, cache=True)
@@ -25,33 +23,28 @@ def shrink_image(img, size=(164, 81)):
     final = img[::3, ::2]
     return final
 
-colors = spatial.cKDTree([
-    [242, 128, 128],    # 0 [240, 240, 240],
-    [196, 141, 197],    # 1 [ 51, 178, 242],
-    [171, 179,  99],    # 2 [216, 127, 229],
-    [186, 135,  93],    # 3 [242, 178, 153],
-    [221, 113, 183],    # 4 [108, 222, 222],
-    [191,  79, 199],    # 5 [ 25, 204, 127],
-    [202, 155, 124],    # 6 [204, 178, 242],
-    [ 82, 128, 128],    # 7 [ 76,  76,  76],
-    [161, 128, 128],    # 8 [153, 153, 153],
-    [152, 112, 108],    # 9 [178, 153,  76],
-    [145, 181,  75],    # a [229, 102, 178],
-    [115, 147,  70],    # b [204, 102,  51],
-    [115, 134, 146],    # c [ 76, 102, 127],
-    [157,  86, 166],    # d [ 78, 166,  87],
-    [128, 179, 155],    # e [ 76,  76, 204],
-    [ 12, 128, 128]     # f [ 17,  17,  17]
-], balanced_tree=False)
-
-
+colors_arr = cv2.cvtColor(np.array([
+    [240, 240, 240],
+    [ 51, 178, 242],
+    [216, 127, 229],
+    [242, 178, 153],
+    [108, 222, 222],
+    [ 25, 204, 127],
+    [204, 178, 242],
+    [ 76,  76,  76],
+    [153, 153, 153],
+    [178, 153,  76],
+    [229, 102, 178],
+    [204, 102,  51],
+    [ 76, 102, 127],
+    [ 78, 166,  87],
+    [ 76,  76, 204],
+    [ 17,  17,  17]
+], dtype='uint8').reshape((16, 1, 3)), color_space).reshape((16, 3))
+colors = spatial.cKDTree(colors_arr, balanced_tree=True)
 
 hex = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
 to_hex = np.vectorize(lambda x: hex[x])
-
-jit_rgb_to_hsluv = jit(hsluv.rgb_to_hsluv, nopython=True, cache=True)
-# @numba.guvectorize(['numba])
-
 
 @jit(forceobj=True, cache=True)
 def image_to_nfp(img):
@@ -76,14 +69,14 @@ if __name__ == '__main__':
         print("Invalid file")
         quit()
 
-    save_path = pathlib.Path(str(movie_path.parent) + "/" + movie_path.stem).absolute()
+    save_path = pathlib.Path("movies/" + movie_path.stem).absolute()
     print("Writing frames to: ", save_path)
 
     import os
     if save_path.exists() and save_path.is_dir():
         import glob
 
-        list_of_files = glob.glob('/path/to/folder/frame_*.nfp') # * means all if need specific format then *.csv
+        list_of_files = glob.glob(str(save_path) + '/frame_*.nfp') # * means all if need specific format then *.csv
         latest_file = max(list_of_files, key=os.path.getctime)
         i = int(''.join(filter(str.isdigit, latest_file)))
     else:
@@ -91,7 +84,7 @@ if __name__ == '__main__':
         i = 0
 
     video = cv2.VideoCapture(str(movie_path))
-    num_frames = 101263
+    num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
     for count in progressbar(range(i, num_frames), min_value=0, max_value=num_frames):
         if not video.isOpened():
@@ -101,7 +94,7 @@ if __name__ == '__main__':
         if not ret:
             break
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+        frame = cv2.cvtColor(frame, color_space)
         shrunk = shrink_image(frame)
         nfp_image = image_to_nfp(shrunk)
         
@@ -109,5 +102,3 @@ if __name__ == '__main__':
             f.write(nfp_image)
 
     video.release()
-
-    import pdb; pdb.set_trace()
